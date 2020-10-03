1c04773401854c1d0eb1e5347b2e33703dc34a16
Kenan Günen <gunenkenan@gmail.com>
2020-10-03 23:34:58 +0300

    create component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TextFlow from './components/text-flow';
 
 function App() {
+  const flowList = ['Ankara', 'İstanbul', 'Samsun', 'İzmir', 'Mersin'];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
+        <TextFlow flowList={flowList} changetime={2000}></TextFlow>
       </header>
     </div>
   );
